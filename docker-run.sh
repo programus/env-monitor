785378f06687f30b3ebb909a74813f48421bf8c4
programus <programus@gmail.com>
2020-10-05 12:44:45 +0900

    added local timezone support

--- docker-run.sh
+++ docker-run.sh
@@ -6,7 +6,17 @@ else
   export DB_IMAGE=mariadb
 fi
 
+if [ -f /etc/timezone ]; then
+  export TZ=`cat /etc/timezone`
+elif [ -h /etc/localtime ]; then
+  export TZ=`readlink /etc/localtime | sed "s/^.*\/zoneinfo\///"`
+else
+  checksum=`md5sum /etc/localtime | cut -d' ' -f1`
+  export TZ=`find /usr/share/zoneinfo/ -type f -exec md5sum {} \; | grep "^$checksum" | sed "s/.*\/zoneinfo\///" | head -n 1`
+fi
+
 echo "using ${DB_IMAGE} as db image."
+echo "timezone: ${TZ}"
 
 export DB_PORT=5506
 
