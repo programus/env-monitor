1fff1a526febefa12ff5286559be6589c83b8ca1
programus <programus@gmail.com>
2020-10-14 23:48:42 +0900

    added default db settings

--- monitor-service/lib/db.js
+++ monitor-service/lib/db.js
@@ -2,11 +2,11 @@ import mysql from 'serverless-mysql'
 
 export const db = mysql({
   config: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5506,
+    database: process.env.DB_NAME || 'envrec',
+    user: process.env.DB_USER || 'envrec',
+    password: process.env.DB_PASSWORD || '3nvRec',
   },
 })
 
