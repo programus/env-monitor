1fe69925973bdeebe90f9bea5750ce7e210ab20d
programus@pi <programus@gmail.com>
2020-10-31 19:09:59 +0900

    updated the log history limit

--- bin/check-availability.sh
+++ bin/check-availability.sh
@@ -3,7 +3,7 @@
 BASE_DIR="$(realpath "$(dirname $0)/..")"
 LOG_FILE="${BASE_DIR}/log/check-availability.log"
 
-REMAIN_LINE=3
+REMAIN_LINE=1000
 
 # call check availability api
 echo "$(date '+%Y-%m-%d %H:%M:%S') - $(curl -X POST http://localhost:3000/api/check-availability -d "{}" -s )" >> "${LOG_FILE}"
